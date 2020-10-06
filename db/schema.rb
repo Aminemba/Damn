faa81ecd370c41c16b2fef9fe499a6fecb1c1048
Amine <amin.mba@iuj.ac.jp>
2020-10-06 13:44:45 +0000

    Adding image and id to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_06_134001) do
+ActiveRecord::Schema.define(version: 2020_10_06_134336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,9 @@ ActiveRecord::Schema.define(version: 2020_10_06_134001) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_link"
+    t.string "provider"
+    t.integer "user_id"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
